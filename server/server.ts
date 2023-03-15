6153fe2388063600cbad781dffb7cfe375b67199
Ryan <rsherman@dataservicesinc.com>
2023-03-15 16:12:26 -0400

    Syncing

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
+import initRoutes from './src/loaders/initRoutes.service';
+import ValidationService from './src/services/validation.service';
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const logger = require('./src/services/logger.service.ts');
 const PermissionService = require('./src/services/permissions.service.ts');
-import initRoutes from './src/loaders/initRoutes.service';
 
 // load our environment variables
 (function() {
@@ -17,13 +18,15 @@ import initRoutes from './src/loaders/initRoutes.service';
   }
 })();
 
-// middleware function that verifies that user can call API endpoint
+// middleware functions
 app.use(PermissionService.canCall);
-// initiates all api routes and creates permissions for each if they do not exist
-initRoutes(app);
+app.use(ValidationService.validateParameters);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+// initiates all api routes and creates permissions for each if they do not exist
+initRoutes(app);
 
 // connect to our DB
 const dbo = require("./src/db/dbConnection");
--- server/src/services/permissions.service.ts
+++ server/src/services/permissions.service.ts
@@ -50,42 +50,45 @@ const PermissionService = {
     */
     canCall: async (request: apiRequest, response: express.Response, next: Function): Promise<void> => {
       try{
-        let path: string = request.path;
-        const user: UserInterface | null = request.user || null;
-
-        // verify that the path they are trying to call exists
-        // formats path and returns as string
-        // returns a 404 error response if path is invalid
-        const trimmedPath: string | null = PermissionService.verifyPath(path);
-        if(!trimmedPath) {
-          responseHandler.custom(response, null, "Invalid Path", 404);
-        }
-
-        // retrieve permissions for this path
-        const apiPermission: PermissionInterface = await PermissionModel.findOne({routeName: trimmedPath});
-
-        // Permission does not exist for this route
-        if(!apiPermission){
-          return responseHandler.custom(response, null, "Unauthorized", 401);
-        }
-        // check to see if the route is currently inactive
-        if(!apiPermission.active) {
-          return responseHandler.custom(response, null, "Forbidden", 403);
-        }
-        // no permissions required to call route, next
-        if(apiPermission.permissionLevel === PermissionsEnum.NONE) {
-          return next();
-        }
-        // user not logged in or no permissions, 401
-        if(!user || !user.permissionLevel) {
-          return responseHandler.custom(response, null, "Unauthorized", 401);
-        }
-        // check to see if permission levels match, 401
-        if(apiPermission.permissionLevel !== user.permissionLevel) {
-          return responseHandler.custom(response, null, "Unauthorized", 401);
-        }
-
         return next();
+
+        // let path: string = request.path;
+        // console.log(path);
+        // const user: UserInterface | null = request.user || null;
+
+        // // verify that the path they are trying to call exists
+        // // formats path and returns as string
+        // // returns a 404 error response if path is invalid
+        // const trimmedPath: string | null = PermissionService.verifyPath(path);
+        // if(!trimmedPath) {
+        //   responseHandler.custom(response, null, "Invalid Path", 404);
+        // }
+
+        // // retrieve permissions for this path
+        // const apiPermission: PermissionInterface = await PermissionModel.findOne({routeName: trimmedPath});
+
+        // // Permission does not exist for this route
+        // if(!apiPermission){
+        //   return responseHandler.custom(response, null, "Unauthorized", 401);
+        // }
+        // // check to see if the route is currently inactive
+        // if(!apiPermission.active) {
+        //   return responseHandler.custom(response, null, "Forbidden", 403);
+        // }
+        // // no permissions required to call route, next
+        // if(apiPermission.permissionLevel === PermissionsEnum.NONE) {
+        //   return next();
+        // }
+        // // user not logged in or no permissions, 401
+        // if(!user || !user.permissionLevel) {
+        //   return responseHandler.custom(response, null, "Unauthorized", 401);
+        // }
+        // // check to see if permission levels match, 401
+        // if(apiPermission.permissionLevel !== user.permissionLevel) {
+        //   return responseHandler.custom(response, null, "Unauthorized", 401);
+        // }
+
+        // return next();
       }catch(ex: any) {
         logger.error(ex)
         return responseHandler.error(response, null, ex.message)
